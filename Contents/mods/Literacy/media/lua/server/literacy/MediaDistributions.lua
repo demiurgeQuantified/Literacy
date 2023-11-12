54188b8bdf75299e3a4a3855efe56969fe3da755
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-11-12 23:04:51 +0000

    fix rare literacy tapes being common when learn to read is disabled

--- Contents/mods/Literacy/media/lua/server/literacy/MediaDistributions.lua
+++ Contents/mods/Literacy/media/lua/server/literacy/MediaDistributions.lua
@@ -18,7 +18,12 @@
 local LiteracyTapes = {}
 LiteracyTapes.Tapes = {}
 
-function LiteracyTapes.getTapes()
+---@type RecordedMedia
+local recMedia
+
+---@param recordedMedia RecordedMedia
+function LiteracyTapes.getTapes(recordedMedia)
+    recMedia = recordedMedia
     for guid,data in pairs(RecMedia) do
         if data.category == 'Literacy-VHS' then
             if not LiteracyTapes.Tapes[data.spawning] then LiteracyTapes.Tapes[data.spawning] = {} end
@@ -28,28 +33,44 @@ function LiteracyTapes.getTapes()
 end
 Events.OnInitRecordedMedia.Add(LiteracyTapes.getTapes)
 
-function LiteracyTapes.setVHS(_roomName, _containerType, container)
+---@param rarity int
+---@return MediaData
+function LiteracyTapes.getRandomTape(rarity)
+    return recMedia:getMediaData(LiteracyTapes.Tapes[rarity][ZombRand(1, #LiteracyTapes.Tapes[rarity]+1)])
+end
+
+---@param _roomName string
+---@param _containerType string
+---@param container ItemContainer
+function LiteracyTapes.setContainerTapes(_roomName, _containerType, container)
     local VHSs = container:FindAll('VHS_Literacy')
     if not VHSs then return end
     for i = 0, VHSs:size()-1 do
+        ---@type InventoryItem
         local item = VHSs:get(i)
+        
         local rarity = 0
+        local remove
         local choice = ZombRand(0, 10)
         if choice > 8 then
             if SandboxVars.Literacy.WantLiteracyVHS then
                 rarity = 3
             else
-                rarity = 2
+                remove = true
             end
         elseif choice > 6 then
             rarity = 2
         elseif choice > 3 then
             rarity = 1
         end
-        local tape = LiteracyTapes.Tapes[rarity][ZombRand(1, #LiteracyTapes.Tapes[rarity]+1)]
-        item:setRecordedMediaData(getZomboidRadio():getRecordedMedia():getMediaData(tape))
+        
+        if remove then
+            container:Remove(item)
+        else
+            item:setRecordedMediaData(LiteracyTapes.getRandomTape(rarity))
+        end
     end
 end
-Events.OnFillContainer.Add(LiteracyTapes.setVHS)
+Events.OnFillContainer.Add(LiteracyTapes.setContainerTapes)
 
 return LiteracyTapes
